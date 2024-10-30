8d494e5f339594577226cfa739a0fdb0ccc2dde8
Alonso <alonso.rbass@gmail.com>
2024-10-30 16:32:56 +0100

    feat: created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import {React} from "react";
+import { React } from "react";
 import bgVideo from "./assets/videos/earth-bg.mp4";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <div>
       <div className="h-[700px] relative">
-        <video 
-        autoPlay 
-        loop muted 
-        className="fixed right-0 top-0 h-[700px] w-full object-cover z[-1]">
+        <video autoPlay loop muted className="fixed right-0 top-0 h-[700px] w-full object-cover z[-1]">
           <source src={bgVideo} type="video/mp4" />
         </video>
+        <Navbar/>
       </div>
     </div>
   )
